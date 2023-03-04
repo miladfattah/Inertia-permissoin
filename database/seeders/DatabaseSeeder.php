8e210a4d5c931a8003636ce958e46cfddc7e2058
MiladFattah <miladalpha1379@gmail.com>
2023-03-04 06:19:37 -0800

    add roles and admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
-
+        $this->call(roleSeeder::class);
+        $this->call(adminSeeder::class);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
