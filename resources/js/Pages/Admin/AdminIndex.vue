c108d65f42c6067e7aa813a8c5008cb01398a747
MiladFattah <miladalpha1379@gmail.com>
2023-03-04 07:01:20 -0800

    edit authlayout and add adminlayout

--- resources/js/Pages/Admin/AdminIndex.vue
+++ resources/js/Pages/Admin/AdminIndex.vue
@@ -1,22 +1,18 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import AdminLayout from '@/Layouts/AdminLayout.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
     <Head title="Dashboard" />
 
-    <AuthenticatedLayout>
+    <AdminLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">Admin welcome in website!</div>
-                </div>
-            </div>
+        <div class="py-4">
+            <h1>Admin welcome!</h1>
         </div>
-    </AuthenticatedLayout>
+    </AdminLayout>
 </template>
